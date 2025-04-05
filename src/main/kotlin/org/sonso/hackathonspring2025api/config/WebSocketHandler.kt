43c26777be53659015303341de609b11d812d00c
kinok0 <truthpruthbg@gmail.com>
2025-04-05 04:17:34 +0300

    Code-quality

--- src/main/kotlin/org/sonso/hackathonspring2025api/config/WebSocketHandler.kt
+++ src/main/kotlin/org/sonso/hackathonspring2025api/config/WebSocketHandler.kt
@@ -2,11 +2,11 @@ package org.sonso.hackathonspring2025api.config
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
 import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
-import org.springframework.stereotype.Component
 import java.util.concurrent.CopyOnWriteArrayList
 
 @Component
