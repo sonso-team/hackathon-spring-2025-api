fb7cd1376eda0766f54f371a25abd26aac150884
kinok0 <truthpruthbg@gmail.com>
2025-04-05 04:15:28 +0300

    Взаимодействие переведено на использование сырого WebSocket

---
+++ src/main/kotlin/org/sonso/hackathonspring2025api/config/WebSocketHandler.kt
@@ -0,0 +1,30 @@
+package org.sonso.hackathonspring2025api.config
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.handler.TextWebSocketHandler
+import org.springframework.stereotype.Component
+import java.util.concurrent.CopyOnWriteArrayList
+
+@Component
+class WebSocketHandler : TextWebSocketHandler() {
+    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+    val sessions = CopyOnWriteArrayList<WebSocketSession>()
+
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        sessions.add(session)
+        logger.info("Подключилась новая сессия: ${session.id}")
+    }
+
+    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
+        sessions.remove(session)
+        logger.info("Сессия закрыта: ${session.id}")
+    }
+
+    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+        logger.info("Получено сообщение от ${session.id}: ${message.payload}")
+    }
+}
